b4c93186e18d1dd0f590413470a6ca10a65e0888
Jakbarov7 <168078269+Jakbarov7@users.noreply.github.com>
2024-04-25 14:40:16 +0500

    yangilandi

--- malumot_turi2.py
+++ malumot_turi2.py
@@ -1,9 +1,9 @@
 import numpy as np
 # Array yaratayotganimizda , uning malumot turini aniq qilib berish
 # arr = np.array([1, 2, 3, 4], dtype=np.float64)
-# .dtype - arrayimiz qaysi ma'lumot turida ekanligini aniqlab beradi
-# print(arr.dtype)
-# print(arr)
+.dtype - arrayimiz qaysi ma'lumot turida ekanligini aniqlab beradi
+print(arr.dtype)
+print(arr)
 
 # Bir ma'lumot turidan boshqasiga o'tish
 arr = np.array([1, 2, 3, 4]) # dtype = int64
